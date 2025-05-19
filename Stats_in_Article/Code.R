5195ad29cba941c61fce34d02fcfc87a7c358f41
Utsav Lamichhane <132714550+utsavlamichhane@users.noreply.github.com>
2025-05-19 16:17:54 -0400

    Update Code.R

--- Stats_in_Article/Code.R
+++ Stats_in_Article/Code.R
@@ -1,22 +1,24 @@
 #Install the Library
+install.packages("mbX")
 
-```
+#load the library
+
 library(mbX)
-```
+
 
 ##Clean at Genus level:**
-```
+
 ezclean("rumen_feces_microbiome.csv",
         "rumen_fecal_metadata.txt",
-        level = "Genus")
-```
+        level = "G")
+
 
 
 #Visualize by SampleType (Rumen vs. Feces), showing the top 10 genera
-```
+
 ezviz("rumen_feces_Genus_abundance.xlsx",
       "rumen_fecal_metadata.txt",
-      level             = "Genus",
+      level             = "G",
       selected_metadata = "SampleType",
       top_taxa          = 10)
-```
+
